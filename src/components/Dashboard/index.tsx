940f79cc80a278895a02923db1c22ddf8c9f87ca
Matheus Cunha <matheus.cunha@seidor.com.br>
2021-06-29 15:30:05 -0300

    component TransactionsTable finish

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,11 +1,13 @@
 // style compoent
 import { Summary } from "../Summary"
+import { TransactionsTable } from "../TransactionsTable"
 import { Container } from "./style"
 
 export function Dashboard() {
 	return (
 		<Container>
 			<Summary />
+			<TransactionsTable />
 		</Container>
 	)
 }
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,34 @@
+// style compoent
+import { Container } from "./style"
+
+export function TransactionsTable() {
+	return (
+		<Container>
+			<table>
+				<thead>
+					<tr>
+						<th>Título</th>
+						<th>Valor</th>
+						<th>Categoria</th>
+						<th>Data</th>
+					</tr>
+				</thead>
+
+				<tbody>
+					<tr>
+						<td>Site vendido</td>
+						<td className="deposit">R$12.000</td>
+						<td>Desenvolvimento</td>
+						<td>20/12/2020</td>
+					</tr>
+					<tr>
+						<td>Computador novo</td>
+						<td className="withdraw">-R$7.000</td>
+						<td>Eletronico</td>
+						<td>20/03/2020</td>
+					</tr>
+				</tbody>
+			</table>
+		</Container>
+	)
+}
