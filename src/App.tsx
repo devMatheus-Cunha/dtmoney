ffae734f6ababc7a24fa01ed06299bf3cd86fff8
Matheus Cunha <matheus.cunha@seidor.com.br>
2021-06-29 09:36:04 -0300

    install eslint and e config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-
 // Style
 import { GlobalStyle } from "./style/global";
 
 export function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <h1>Hora de ficar rico</h1>
-    </>
-  );
+	return (
+		<>
+			<GlobalStyle />
+			<h1>Hora de ficar rico</h1>
+		</>
+	);
 }
