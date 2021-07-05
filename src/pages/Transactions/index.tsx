334befb4935eae5a473255b39744ab74f97ca9a8
Matheus Gomes <gomesprogramacao@gmail.com>
2021-07-05 19:37:19 -0300

    cleaning dead code

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,30 +1,16 @@
-import { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 // components
 import { Dashboard } from "../../components/Dashboard";
 import { Header } from "../../components/Header";
 import { NewTransactionModal } from "../../components/NewTransactionModal";
 import { TransactionsTable } from "../../components/TransactionsTable";
-import { AuthContext } from "../../contexts/AuthContext";
 
 import { GlobalStyle } from "../../style/global";
 
-type TransactionsParams = {
-  id: string
-}
-
 export function Transactions() {
-	const { user } = useContext(AuthContext)
-
   	// state
 	const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
-	const params = useParams<TransactionsParams>()
-	const [idTransaciton, setIdTransaciton] = useState("")
-
-	useEffect(() => {
-		setIdTransaciton(params.id)
-	}, [params.id])
 
 	function handleOpenNewTransaction() {
 		setIsNewTransactionModalOpen(true)
